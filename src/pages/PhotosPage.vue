83a833c70159ed2a90813ca807ea8197d6fb535b
joshkrab <joshkrab@gmail.com>
2022-12-26 14:29:54 +0200

    photo00

--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,6 +1,8 @@
 <template>
 	<v-container>
-		<Photo />
+		<v-row>
+			<Photo v-for="photo in photos" v-bind:photo="photo" />
+		</v-row>
 	</v-container>
 </template>
 <script>
@@ -10,14 +12,25 @@ export default {
 	components: { Photo },
 
 	data: () => ({
-		photos: [
-			{ id: 1, title: 'photo 1' },
-			{ id: 2, title: 'photo 2' },
-			{ id: 3, title: 'photo 3' },
-			{ id: 4, title: 'photo 4' },
-			{ id: 5, title: 'photo 5' },
-		]
-	})
+		// photos: [
+		// 	{ id: 1, title: 'photo 1' },
+		// 	{ id: 2, title: 'photo 2' },
+		// 	{ id: 3, title: 'photo 3' },
+		// 	{ id: 4, title: 'photo 4' },
+		// 	{ id: 5, title: 'photo 5' },
+		// ]
+		photos: []
+	}),
+
+	mounted () {
+		this.fetchToDo()
+	},
+
+	methods: {
+		fetchToDo () {
+			this.axios.get('https://jsonplaceholder.typicode.com/photos?_limit=10').then(response => this.photos = response.data)
+		}
+	}
 }
 </script>
 <style>
