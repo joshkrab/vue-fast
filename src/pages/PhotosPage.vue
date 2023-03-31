51875148766b2500737882fa92f43f1fa9a5e117
Ihor Neizvestnyi <ihor.neizvestnyi@novait.local>
2023-03-31 16:37:07 +0300

    ddd

--- src/pages/PhotosPage.vue
+++ src/pages/PhotosPage.vue
@@ -1,9 +1,9 @@
 <template>
 	<v-container>
-		<PhotoForm v-if='photos.length < 11' @addPhoto="addPhoto" />
-		<div v-else>You can't add more photos</div>
+		<PhotoForm @addPhoto="addPhoto" />
+		<!-- <div v-else>You can't add more photos</div> -->
 		<v-row>
-			<Photo v-for="photo in photos" v-bind:photo="photo" @openPhoto="openPhoto" />
+			<Photo v-for="photo in $store.getters.getAllPhotos" v-bind:photo="photo" @openPhoto="openPhoto" />
 		</v-row>
 		<PhotoDialog :photo="currentPhoto" v-model="dialogVisible" />
 	</v-container>
@@ -33,14 +33,15 @@ export default {
 	}),
 
 	mounted () {
-		this.fetchToDo()
+		// this.fetchToDo()
+		this.$store.dispatch('fetchPhotos')
 	},
 
 	methods: {
-		fetchToDo () {
-			this.axios.get('https://jsonplaceholder.typicode.com/photos?_limit=10')
-				.then(response => this.photos = response.data)
-		},
+		// fetchToDo () {
+		// 	this.axios.get('https://jsonplaceholder.typicode.com/photos?_limit=10')
+		// 		.then(response => this.photos = response.data)
+		// },
 		addPhoto (photo) {
 			this.photos.push(photo)
 		},
